dd8d619d92d75b38165ae29436b9be9bd7a52fc1
Nilesh Bharambe <nileshbharambe26@gmail.com>
2023-03-01 19:33:07 +0530

    initial commit

---
+++ src/main/java/com/repository/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.model.Movie;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, Integer> {
+
+}
---
+++ src/main/java/com/service/MovieService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.model.Movie;
+
+@Service
+public interface MovieService {
+	
+	String welcomeMsg();
+	public List<String[]>  getMovies();
+	public Movie getMovieById(String id);
+	public Movie insertMovie(Movie movie);
+}
